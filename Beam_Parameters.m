3f2eff21ef73b65b148f6fea720ff2e41c887c35
Marcos Hernan Garcia <marcos.h.garcia@gmail.com>
2023-06-01 17:30:07 -0300

    Se sube el proyecto

---
+++ Beam_Parameters.m
@@ -0,0 +1,10 @@
+%Parametros del sistema
+%   21/05/2023
+%   Almude,Chrino,Garcia
+
+m = 0.1   % Masa de la pelota [kg]
+R = 0.15  % Radio de la pelota [m]
+L = 3       % largo de la barra [m]
+mb= 0.225   % masa de la barra [kg]
+J = 1/12*mb*L^2 % momento de inercia de la barra [kg*m^2]
+g = 9.85    % aceletacion de la gravedad [m/s^2]
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # BallAndBeamFuzzyControl
+
+Para correr el proyecto:
+
+1) Importar los parametros fisicos:
+
+`Beam_parameters`
+
+2) Exportar al workspace las reglas:
+
+`fizzy('FCbeam_30_reglas_v2')`
+
+3) Abrir y simular el diagrama:
+
+`slcontrolbeam.mdl`
